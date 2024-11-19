8cb24d9446d069c918e94bd02e3718a714096c42
Dubravko <dev.richard.npm98@gmail.com>
2024-11-19 22:33:46 +0700

    Update

--- Model/User.js
+++ Model/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const moment = require('moment');
+const moment = require("moment");
 const createdAt = moment().format();
 const UserSchema = mongoose.Schema({
   _id: mongoose.Types.ObjectId,
@@ -7,8 +7,8 @@ const UserSchema = mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: Number, default: 0 }, //  0 = user , 1 = admin
-  avatar: { type: String, default: "https://res.cloudinary.com/phuockaito/image/upload/v1617902959/user/1_gxwhfk.jpg" },
-  createdAt: { type: String, default: createdAt }
+  avatar: { type: String, default: "https://res.cloudinary.com/dfrdxqinp/image/upload/v1731941970/test/xu1pdiyosvnhq5jt2fqo.jpg" },
+  createdAt: { type: String, default: createdAt },
 });
-const User = mongoose.model('user', UserSchema);
+const User = mongoose.model("user", UserSchema);
 module.exports = User;
